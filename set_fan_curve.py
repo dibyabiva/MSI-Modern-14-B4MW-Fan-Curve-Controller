5ee523f5d64c98b8e75b14328977abbc4ddbbcb0
Dibyabiva Seth <meetdseth@gmail.com>
2021-09-20 23:10:08 +0530

    Update set_fan_curve.py

--- set_fan_curve.py
+++ set_fan_curve.py
@@ -15,26 +15,26 @@ while(True):
         
     # GPU Temp - Fan Curve (0% - 150%) 
     print("Setting GPU Fan Curve...")
-    data[114] = gpu_fan_speeds[0] # Lowest temperature fan speed %
-    data[115] = gpu_fan_speeds[1]
-    data[116] = gpu_fan_speeds[2]
-    data[117] = gpu_fan_speeds[3]
-    data[118] = gpu_fan_speeds[4]
-    data[119] = gpu_fan_speeds[5] # Highest temperature fan speed %
+    data[114] = gpu_fan_speeds[0] # Address:0x72 (Lowest temperature fan speed %)
+    data[115] = gpu_fan_speeds[1] # Address:0x73
+    data[116] = gpu_fan_speeds[2] # Address:0x74
+    data[117] = gpu_fan_speeds[3] # Address:0x75
+    data[118] = gpu_fan_speeds[4] # Address:0x76
+    data[119] = gpu_fan_speeds[5] # Address:0x77 (Highest temperature fan speed %)
 
 
     # CPU Temp - Fan Curve (0% - 150%) 
     print("Setting CPU Fan Curve...")
-    data[138] = cpu_fan_speeds[0] # Lowest temperature fan speed %
-    data[139] = cpu_fan_speeds[1]
-    data[140] = cpu_fan_speeds[2]
-    data[141] = cpu_fan_speeds[3]
-    data[142] = cpu_fan_speeds[4]
-    data[143] = cpu_fan_speeds[5] # Highest temperature fan speed %
+    data[138] = cpu_fan_speeds[0] # Address:0x8A (Lowest temperature fan speed %)
+    data[139] = cpu_fan_speeds[1] # Address:0x8B
+    data[140] = cpu_fan_speeds[2] # Address:0x8C
+    data[141] = cpu_fan_speeds[3] # Address:0x8D
+    data[142] = cpu_fan_speeds[4] # Address:0x8E
+    data[143] = cpu_fan_speeds[5] # Address:0x8F (Highest temperature fan speed %)
 
     # Fan Profile Mode
     print("Setting Fan Profile...")
-    data[244] = 140
+    data[244] = 140 # Address:0xF4
 
     print("Writing data to EC Memory...")
     with open(ec_filename, "wb") as ec_file:
